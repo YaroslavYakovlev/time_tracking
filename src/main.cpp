9079fc1d0df07f1c73fb0788bdd015f8840db4f6
Yaroslav Yakovlev <ekb-buza@mail.ru>
2022-05-22 21:06:56 +0500

    решение

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,10 @@ int main() {
   const std::string exit = "exit";
   std::map<std::string, std::time_t> archive_task;
   std::string command = "";
-  std::string name_task = "";
   std::string new_name_task = "";
+  std::string name_task = "";
   std::time_t time_start = 0;
+  std::time_t time_start_old = 0;
   std::time_t time_end = 0;
   std::time_t time_spent = 0;
   bool first_task = true;
@@ -26,33 +27,31 @@ int main() {
     std::cin >> command;
     if (!command.empty()) {
       if (command == begin) {
-        std::cout << "begin" << std::endl;
         std::cout << "Enter the name of the task" << std::endl;
         std::cin >> new_name_task;
+        time_start_old = time_start;
         count_task++;
         time_start = std::time(nullptr);
-        std::cout << "time_start " << time_start << std::endl;
-        /**
-         * TODO Счет времени при начало task и началом нового task = 0
-         **/
-        if (count_task > 1)
-        // if ((name_task != new_name_task))
-        {
-          std::cout << "Task " << name_task << " finish." << std::endl;
-          std::cout << "The task began " << new_name_task << std::endl;
-          time_spent = time_end - time_start;
+        // if (count_task == 1) {
+        //   name_task = new_name_task;
+        //   // archive_task.push_back(name_task);
+        // }
+        if (count_task >= 1) {
+          time_end = std::time(nullptr);
+          time_spent = time_end - time_start_old;
           std::cout << "time_spent " << time_spent << std::endl;
-          archive_task.insert(std::make_pair(new_name_task, time_spent));
-          name_task = new_name_task;
-        } else {
-          time_spent = time_end - time_start;
-          std::cout << "time_spent " << time_spent << std::endl;
+          if (count_task > 1) {
+            std::cout << "Task " << name_task << " finish." << std::endl;
+            std::cout << "The task began " << new_name_task << std::endl;
+
+            name_task = new_name_task;
+          }
           archive_task.insert(std::make_pair(new_name_task, time_spent));
         }
-        task = true;
+        // task = true;
       } else if (command == end) {
         time_end = std::time(nullptr);
-        time_spent = time_end - time_start;
+        time_spent = time_end - time_start_old;
         archive_task.insert(std::make_pair(name_task, time_spent));
       } else if (command == status) {
         for (std::map<std::string, std::time_t>::iterator it = archive_task.begin(); it != archive_task.end(); it++) {
@@ -66,8 +65,7 @@ int main() {
     } else {
       std::cout << "The team cannot be empty" << std::endl;
     }
-    time_end = std::time(nullptr);
-    std::cout << "time_end " << time_end << std::endl;
+
   } while (true);
   for (std::map<std::string, std::time_t>::iterator it = archive_task.begin(); it != archive_task.end(); it++) {
     std::cout << "Task - " << it->first << "; Time - " << it->second << std::endl;
